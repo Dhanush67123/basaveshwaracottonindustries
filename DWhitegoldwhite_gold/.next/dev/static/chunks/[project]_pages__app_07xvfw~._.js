2769982f64402cb6d38cda01f58651a2dcd46863
Dhnaush <gdhanush093@gmail.com>
2026-06-17 18:22:51 +0530

    chore: clean up accidental build artifacts and update lockfile

--- DWhitegoldwhite_gold/.next/dev/static/chunks/[project]_pages__app_07xvfw~._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/0rr9_next_dist_compiled_0.h~90_._.js","static/chunks/0rr9_next_dist_shared_lib_12r8god._.js","static/chunks/0rr9_next_dist_client_0igrydu._.js","static/chunks/0rr9_next_dist_0990-w.._.js","static/chunks/12t3_next_039nf.t._.js","static/chunks/[next]_entry_page-loader_ts_0j~flwh._.js","static/chunks/0rr9_react-dom_0bsdzvp._.js","static/chunks/0rr9_06jqo6f._.js","static/chunks/[root-of-the-server]__0c0okpg._.js"],
-    source: "entry"
-});
